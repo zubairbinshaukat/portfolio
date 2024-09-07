f57911c57212fc5f8e3bc3a77495bd70d57c1c63
zubairbinshaukat <zubairbinshaukat4455@gmail.com>
2024-09-07 15:54:03 +0500

    Projects updated

---
+++ app/loading.js
@@ -0,0 +1,14 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="w-screen bg-black h-screen flex justify-center items-center">
+        <div class="newtons-cradle">
+          <div class="newtons-cradle__dot"></div>
+          <div class="newtons-cradle__dot"></div>
+          <div class="newtons-cradle__dot"></div>
+          <div class="newtons-cradle__dot"></div>
+        </div>
+      </div>
+    </>
+  )
+}
--- components/ui/tabs.js
+++ components/ui/tabs.js
@@ -89,7 +89,7 @@ export const FadeInDiv = ({
           animate={{
             y: isActive(tab) ? [0, 40, 0] : 0,
           }}
-          className={cn("w-full h-full absolute top-0 left-0", className)}>
+          className={cn("w-full h-[30rem] absolute top-0 left-0", className)}>
           {tab.content}
         </motion.div>
       ))}
